f5d1f510d7bd7e6932e6c5313811e739417a7f32
Barnabas Bulpett <bbulpett@charter.net>
2015-09-22 18:45:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HeyYou::Application.config.secret_token = '47b7119ae395bab558be34f371f3c6b9f06fa23cda360212772e4c49d95eccd00abd65470cecf01cf1580ed59fbd2c23676d6a3d3427536a9d72e768b986e13c'
